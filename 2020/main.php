ebe6d680feb5e45ef3d0f674ce865d6995ef7e45
Ariel-Hidalgo <arielhidalgo808@gmail.com>
2020-04-01 17:35:01 -0300

    Agregando un "hola mundo!"

--- 2020/main.php
+++ 2020/main.php
@@ -1,4 +1,12 @@
 <?php
+ // Ejercicio #1
     for ($i = 0; $i < 10; $i++){
         echo $i;
     }
+
+ // Imprimiendo hola mundo
+
+echo "<p>";                   //Esto agregará un salto de línea
+echo "Hola mundo, soy Ariel!";
+
+?>
