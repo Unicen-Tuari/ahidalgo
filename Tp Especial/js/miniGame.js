56c0682728efcc2e2ec86ccb8543ddd1ad771e87
Ariel-Hidalgo <arielhidalgo808@gmail.com>
2020-06-14 14:37:21 -0300

    funcion de generar personajes funciona n veces

--- Tp Especial/js/miniGame.js
+++ Tp Especial/js/miniGame.js
@@ -6,21 +6,27 @@ btnPersonajes.addEventListener("click", personajes);
 let btnBatalla = document.getElementById("botonBatalla");
 btnBatalla.addEventListener("click",resultado);
 
-function nodoPersonajes(personaje1,personaje2){
-    let nodoPers1 = document.getElementById("personaje1");
-    nodoPers1.innerHTML = personaje1;
+let arrPers = ["MIDORIYA", "BAKUGO", "TODOROKI", "URARAKA"];
 
-    let nodoPers2 = document.getElementById("personaje2");
-    nodoPers2.innerHTML = personaje2;
-}
+   let numeroRandom1 = getRandom(1,100);
+        let arreglo1 = [personaje1,numeroRandom1];
+
+    let numeroRandom2 = getRandom(1,100);
+        let arreglo2 = [personaje2,numeroRandom2];
 
 function personajes(){
+    let personaje1 = arrPers[Math.floor(Math.random() * arrPers.length)];
+    let personaje2 = arrPers[Math.floor(Math.random() * arrPers.length)];
     nodoPersonajes(personaje1,personaje2);
 }
 
-let arrPers = ["MIDORIYA", "BAKUGO", "TODOROKI", 
-"URARAKA"];
-console.log(arrPers);
+function nodoPersonajes(p1,p2){
+    let nodoPers1 = document.getElementById("personaje1");
+    nodoPers1.innerHTML = p1;
+
+    let nodoPers2 = document.getElementById("personaje2");
+    nodoPers2.innerHTML = p2;
+}
 
 function combate(arreglo1,arreglo2){
     let nodoResultado = document.getElementById("resultado");
@@ -41,14 +47,6 @@ function combate(arreglo1,arreglo2){
 function getRandom(min, max){
     return Math.floor(Math.random() * (max - min)) + min;
 }
-
-let personaje1 = arrPers[Math.floor(Math.random() * arrPers.length)];
-   let numeroRandom1 = getRandom(1,100);
-        let arreglo1 = [personaje1,numeroRandom1];
-
-let personaje2 = arrPers[Math.floor(Math.random() * arrPers.length)];
-    let numeroRandom2 = getRandom(1,100);
-        let arreglo2 = [personaje2,numeroRandom2];
 
 function resultado(){
     combate(arreglo1,arreglo2);
