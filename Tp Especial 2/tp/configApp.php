f60edd94531f69d03e762573ec42524160377849
Ariel-Hidalgo <arielhidalgo808@gmail.com>
2020-06-16 10:53:42 -0300

    agregando route avanzado y seccion admin

---
+++ Tp Especial 2/tp/configApp.php
@@ -0,0 +1,18 @@
+<?php
+
+    class configApp{
+    public static $ACTION = "action";
+    public static $PARAMS = "params";
+    public static $ACTIONS = [
+        'home' => 'index',
+        'personajes' => 'personajes',
+        'contacto' => 'contacto',
+        'creadores' => 'creadores',
+        'curiosidades' => 'curiosidades',
+        'veronline' => 'verOnline',
+        'juego' => 'juego',
+        'admin' => 'admin',
+    ];
+}
+
+?>
---
+++ Tp Especial 2/tp/routeAvanzado.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once "configApp.php";
+
+require_once "index.php";
+require_once "characters.php";
+require_once "contacto.php";
+require_once "creadores.php";
+require_once "curiosidades.php";
+require_once "indexOnline.php";
+require_once "juego.php";
+require_once "admin.php";
+
+function parseUrl($url){
+    $partesUrl = explode("/" , $url);
+
+$arrayReturn[configApp::$ACTION] = $partesUrl[0];
+$arrayReturn[configApp::$PARAMS] = isset($partesUrl[1]) ? array_slice($partesUrl,1) : null;
+
+return $arrayReturn;
+
+}
+
+$urlData = parseUrl($_GET[configApp::$ACTION]);
+$actionName = $urlData[configApp::$ACTION];
+if(array_key_exists($actionName,configApp::$ACTIONS)){
+    $params = $urlData[configApp::$PARAMS];
+    $methodName = configApp::$ACTIONS[$actionName];
+
+    if(isset($params) && $params != null){
+        echo $methodName($params);
+    }else{
+        echo $methodName();
+    }
+}else{
+    echo index();
+}
+?>
