d6092ce2ab5bfaff7de22f4c37926e4976ddc91c
ariel-hidalgo <arielhidalgo808@gmail.com>
2020-07-04 18:36:34 -0300

    detalles en bbdd (session is working again)

--- Tp Especial 2/tp/db/characters.sql
+++ Tp Especial 2/tp/db/characters.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-07-2020 a las 22:51:03
+-- Tiempo de generación: 04-07-2020 a las 23:34:17
 -- Versión del servidor: 10.4.13-MariaDB
 -- Versión de PHP: 7.4.7
 
@@ -37,7 +37,8 @@ CREATE TABLE `bloodtype` (
 --
 
 INSERT INTO `bloodtype` (`id_blood`, `type_blood`) VALUES
-(1, 'A');
+(1, 'A'),
+(2, 'b');
 
 -- --------------------------------------------------------
 
@@ -47,9 +48,16 @@ INSERT INTO `bloodtype` (`id_blood`, `type_blood`) VALUES
 
 CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
-  `email` varchar(150) COLLATE utf8_bin NOT NULL,
+  `mail` varchar(150) COLLATE utf8_bin NOT NULL,
   `password` varchar(150) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id_user`, `mail`, `password`) VALUES
+(1, 'ariel@ariel.com', '$2y$10$1RtguZflFAm91yq3yxElCOOndSPeMp1.bHgWnpfwy3fjNJGmbMqDW');
 
 -- --------------------------------------------------------
 
@@ -67,6 +75,13 @@ CREATE TABLE `_character` (
   `quirk` varchar(50) COLLATE utf8_bin NOT NULL,
   `char_blood` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Volcado de datos para la tabla `_character`
+--
+
+INSERT INTO `_character` (`id_character`, `name`, `description`, `hero_name`, `birthday`, `height`, `quirk`, `char_blood`) VALUES
+(17, 'Izuku Midoriya', 'ola ahre', 'Deku', '15 de julio', '172cm', 'One For All', 2);
 
 --
 -- Índices para tablas volcadas
@@ -99,19 +114,19 @@ ALTER TABLE `_character`
 -- AUTO_INCREMENT de la tabla `bloodtype`
 --
 ALTER TABLE `bloodtype`
-  MODIFY `id_blood` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_blood` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `_character`
 --
 ALTER TABLE `_character`
-  MODIFY `id_character` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_character` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- Restricciones para tablas volcadas
